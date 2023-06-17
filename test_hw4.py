c54ce93662516e75f951e2c2db3c405d3dd78b83
Xalios <mastep@yandex.ru>
2023-06-17 22:37:14 +0300

    Add outputs

--- test_hw4.py
+++ test_hw4.py
@@ -6,6 +6,7 @@ def test_greeting():
     age = 25
     # TODO Сформируйте нужную строку
     output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
@@ -37,10 +38,12 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = PI * r**2
+    print(f"Area: {area}")
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = 2 * PI * r
+    print(f"Length: {length}")
     assert length == 144.51326206513048
 
 
@@ -81,7 +84,8 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = dict(zip(first,second))
-
+    d = dict(zip(first, second))
+    for value in d.values():
+        print(value)
     assert isinstance(d, dict)
     assert len(d) == 5
